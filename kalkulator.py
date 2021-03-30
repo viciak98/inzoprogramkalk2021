501456d1e653a9a9ed616c53c45c3f998a21c3c7
Wiktor <wiktorcybulski72@gmail.com>
2021-03-30 22:21:54 +0200

    problem rozwiązany

--- kalkulator.py
+++ kalkulator.py
@@ -1,20 +1,14 @@
-<<<<<<< HEAD
 def get_info():
 print("Witaj, to jest prosty kalkulator")
 
-def dodaj():
-    print('Wprowadz dwie liczby a zostaną one dodane.')
-    a = int(input())
-    b = int(input())
-    print(a + b)
-    
-get_info()
-dodaj()
-=======
 def dodaj(a, b):
     wynik = a + b
     return wynik
+    
+get_info()    
+    
 a = int(input())
 b = int(input())
 print(dodaj(a, b))
->>>>>>> naprawa_funkcji_dodawania
+
+
